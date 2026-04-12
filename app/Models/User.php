819cbec50c8fd319ccd7f47a87e0b5032a0e858c
Sedale <sedalep12@gmail.com>
2026-04-12 19:22:24 -0400

    added migrations and DB support

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,16 +17,26 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+   protected $fillable = [ 
+        'name',
+        'email',
+        'location',
+        'biography',
+        'password',
+        'photos',    
+   ];
+
+   protected $hidden = [
+        'password',
+   ];
+
+   public function services ()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+          return $this->hasMany(Service::class);
+    }
+
+    public function favourites()
+    {
+        return $this->hasMany(Favourite::class);
     }
 }
---
+++ database/migrations/2026_04_12_195158_create_favourites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favourites', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            // Ensure a user can only favourite a service once           
+            $table->unique(['user_id', 'service_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favourites');
+    }
+};
